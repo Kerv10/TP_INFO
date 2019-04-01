ec37786dcefd93f56580a8bee98246ec76df791e
Kerv10 <c18kadjo@telecom-bretagne.eu>
2019-04-01 19:45:56 +0200

    TEST GITHUB ECLIPSE

---
+++ TEST GITHUB/src/Perite/Add.java
@@ -0,0 +1,36 @@
+package Perite;
+
+
+public class Add {
+	
+	int numA=1;
+	int numB=0;
+	int result;
+	
+	public Add() {
+		this.result=numA+numB;
+		
+	}
+	
+	public int Adition(int a, int b){
+			
+		return a+b;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Add resultat= new Add();
+		System.out.println(resultat.Adition(5,5));
+
+		System.out.println(resultat.result);
+		System.out.println(resultat.result);
+		System.out.println(resultat.result);
+		
+		
+	}
+
+}
---
+++ TEST GITHUB/src/Perite/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author kerv
+ *
+ */
+package Perite;
